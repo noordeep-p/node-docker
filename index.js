ac20b9f5a58b60c59dd44150826c0597d50dbf5d
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-04-25 23:49:13 -0400

    connect to mongodb server using mongoose

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
+const {
+  MONGO_IP,
+  MONGO_PORT,
+  MONGO_USER,
+  MONGO_PASSWORD,
+} = require("./config/config");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(
+    `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+  )
+  .then(() => {
+    console.log(`succesfully connected to db`);
+  })
+  .catch((e) => {
+    console.log(`we have an error: ${e}`);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
